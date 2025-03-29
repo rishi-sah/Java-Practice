6e2c71800e8dc183190ca26cdc800d84c5b970bb
Rishi Sah <rishisah9595@gmail.com>
2025-03-29 20:27:09 +0530

    loops concept or print 10 natural numbers

---
+++ loops.java
@@ -0,0 +1,13 @@
+
+//understanding for loops concept by printing first 10 natural numbers both horizontally and vertically
+import java.util.*;
+
+public class loops {
+    public static void main(String[] args) {
+        int i;
+        for (i = 0; i < 10; i++) {
+            System.out.print(i + " ");// if add println and inside print only i ie,println(i); then vertically
+        }
+
+    }
+}
