531b7a4c1538d5bd6e8294a8608df0568caffd2c
Rishi Sah <rishisah9595@gmail.com>
2025-03-29 18:45:56 +0530

    odd even commit

--- demo.java
+++ demo.java
@@ -1,5 +1,18 @@
-class demo{
+import java.util.*;
+
+public class demo{
     public static void main(String []args){
-        System.out.println("Rishi");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the The Number to check odd or even");
+        int n=sc.nextInt();
+        if(n%2==0){
+            System.out.println(n+"Is even");
+
+            
+        }
+        else{
+            System.out.println(n+ "is odd");
+        }
+
     }
 }
