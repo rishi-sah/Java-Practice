28c329f449b06dc2cbc79c29b3c04d448e7aa7a9
Rishi Sah <rishisah9595@gmail.com>
2025-03-30 19:17:48 +0530

    solidRectangle pattern

---
+++ solidRectangle.java
@@ -0,0 +1,18 @@
+//printing solid rectangle pattern using java
+import java.util.*;
+
+public class solidRectangle {
+    public static void main(String[] args) {
+        int n = 4;
+        int m = 5;
+        int i;
+        int j;
+        for (i = 1; i <= n; i++) {
+            for (j = 1; j <= m; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
